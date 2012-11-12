4b7b7b66c8fd50014edb67743bb4e3fba2346284
Marc Buils <marc.buils@gmail.com>
2012-11-12 16:57:05 +0100

    v0.6.1

--- jquery.flippage.js
+++ jquery.flippage.js
@@ -477,30 +477,38 @@
 		} );
 	};
 	
-	$( function(){
-		$(document).bind( "touchstart touchmove touchend touchcancel", function(event){
-		    var touches = event.changedTouches,
-		        first = touches[0],
-		        type = "";
-		         switch(event.type)
-		    {
-		        case "touchstart": type = "mousedown"; break;
-		        case "touchmove":  type="mousemove"; break;        
-		        case "touchend":   type="mouseup"; break;
-		        default: return;
-		    }
-
-		    var simulatedEvent = document.createEvent("MouseEvent");
-		    simulatedEvent.initMouseEvent(type, true, true, window, 1, 
-		                              first.screenX, first.screenY, 
-		                              first.clientX, first.clientY, false, 
-		                              false, false, false, 0/*left*/, null);
-
-		    first.target.dispatchEvent(simulatedEvent);
-		    
-		    if ( $('.flippage_flipping').size() > 0 ) {
-		    	event.preventDefault();
-		    }
-		} );
-	} );
+	if ( typeof(document.addEventListener) != undefined ){
+		$(document).ready(function(){
+			function touchHandler(event)
+			{
+			    var touches = event.changedTouches,
+			        first = touches[0],
+			        type = "";
+			         switch(event.type)
+			    {
+			        case "touchstart": type = "mousedown"; break;
+			        case "touchmove":  type="mousemove"; break;        
+			        case "touchend":   type="mouseup"; break;
+			        default: return;
+			    }
+	
+			    var simulatedEvent = document.createEvent("MouseEvent");
+			    simulatedEvent.initMouseEvent(type, true, true, window, 1, 
+			                              first.screenX, first.screenY, 
+			                              first.clientX, first.clientY, false, 
+			                              false, false, false, 0/*left*/, null);
+	
+			    first.target.dispatchEvent(simulatedEvent);
+			    
+			    if ( $('.flippage_flipping').size() > 0 ) {
+			    	event.preventDefault();
+			    }
+			}
+	
+		    document.addEventListener("touchstart", touchHandler, true);
+		    document.addEventListener("touchmove", touchHandler, true);
+		    document.addEventListener("touchend", touchHandler, true);
+		    document.addEventListener("touchcancel", touchHandler, true);    
+		});	
+	}
 })(jQuery);
